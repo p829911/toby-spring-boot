2b082f3a5a7521db7b22216dd3825325c71003fe
p829911 <p829911@gmail.com>
2024-05-05 23:16:01 +0900

    introduce front controller

--- src/main/java/me/p829911/helloboot/HellobootApplication.java
+++ src/main/java/me/p829911/helloboot/HellobootApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -20,19 +21,25 @@ public class HellobootApplication {
             servletContext ->
                 servletContext
                     .addServlet(
-                        "hello",
+                        "frontcontroller",
                         new HttpServlet() {
                           @Override
                           protected void service(HttpServletRequest req, HttpServletResponse resp)
                               throws IOException {
-                            String name = req.getParameter("name");
+                            // 인증, 보안, 다국어, 공통 기능
+                            if (req.getRequestURI().equals("/hello") && req.getMethod().equals(
+                                HttpMethod.GET.name())) {
+                              String name = req.getParameter("name");
 
-                            resp.setStatus(HttpStatus.OK.value());
-                            resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                            resp.getWriter().println("Hello " + name);
+                              resp.setStatus(HttpStatus.OK.value());
+                              resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                              resp.getWriter().println("Hello " + name);
+                            } else {
+                              resp.setStatus(HttpStatus.NOT_FOUND.value());
+                            }
                           }
                         })
-                    .addMapping("/hello"));
+                    .addMapping("/*"));
     webServer.start();
   }
 }
