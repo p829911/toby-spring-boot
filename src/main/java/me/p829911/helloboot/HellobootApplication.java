5f48aeb4cb50c3965a964c7d5b3c82e06f01e02c
p829911 <p829911@gmail.com>
2024-05-05 22:44:46 +0900

    start tomcat manually

---
+++ src/main/java/me/p829911/helloboot/HellobootApplication.java
@@ -0,0 +1,16 @@
+package me.p829911.helloboot;
+
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
+public class HellobootApplication {
+
+	public static void main(String[] args) {
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = serverFactory.getWebServer();
+		webServer.start();
+	}
+
+}
