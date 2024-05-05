77f76ca65b37ba2dc82a972ab59c767bc2d91976
p829911 <p829911@gmail.com>
2024-05-06 00:12:48 +0900

    mapping hellocontroller using front controller

--- src/main/java/me/p829911/helloboot/HelloController.java
+++ src/main/java/me/p829911/helloboot/HelloController.java
@@ -1,12 +1,8 @@
 package me.p829911.helloboot;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 public class HelloController {
-	@GetMapping("/hello")
 	public String hello(@RequestParam String name) {
 		return "Hello " + name;
 	}
--- src/main/java/me/p829911/helloboot/HellobootApplication.java
+++ src/main/java/me/p829911/helloboot/HellobootApplication.java
@@ -19,27 +19,32 @@ public class HellobootApplication {
     WebServer webServer =
         serverFactory.getWebServer(
             servletContext ->
-                servletContext
-                    .addServlet(
-                        "frontcontroller",
-                        new HttpServlet() {
-                          @Override
-                          protected void service(HttpServletRequest req, HttpServletResponse resp)
-                              throws IOException {
-                            // 인증, 보안, 다국어, 공통 기능
-                            if (req.getRequestURI().equals("/hello") && req.getMethod().equals(
-                                HttpMethod.GET.name())) {
-                              String name = req.getParameter("name");
+            {
+              HelloController helloController = new HelloController();
+              servletContext
+                  .addServlet(
+                      "frontcontroller",
+                      new HttpServlet() {
+                        @Override
+                        protected void service(HttpServletRequest req, HttpServletResponse resp)
+                            throws IOException {
+                          // 인증, 보안, 다국어, 공통 기능
+                          if (req.getRequestURI().equals("/hello")
+                              && req.getMethod().equals(HttpMethod.GET.name())) {
+                            String name = req.getParameter("name");
+                            String ret = helloController.hello(name);
 
-                              resp.setStatus(HttpStatus.OK.value());
-                              resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                              resp.getWriter().println("Hello " + name);
-                            } else {
-                              resp.setStatus(HttpStatus.NOT_FOUND.value());
-                            }
+                            resp.setStatus(HttpStatus.OK.value());
+                            resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                            resp.getWriter().println(ret);
+                          } else {
+                            resp.setStatus(HttpStatus.NOT_FOUND.value());
                           }
-                        })
-                    .addMapping("/*"));
+                        }
+                      })
+                  .addMapping("/*");
+            });
+
     webServer.start();
   }
 }
