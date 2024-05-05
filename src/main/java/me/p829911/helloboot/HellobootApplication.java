50835731c7c1146a600f33355cdd9edd54083d94
p829911 <p829911@gmail.com>
2024-05-05 23:03:19 +0900

    register servlet

--- src/main/java/me/p829911/helloboot/HellobootApplication.java
+++ src/main/java/me/p829911/helloboot/HellobootApplication.java
@@ -1,16 +1,33 @@
 package me.p829911.helloboot;
 
-
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
 public class HellobootApplication {
 
-	public static void main(String[] args) {
-		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer();
-		webServer.start();
-	}
-
+  public static void main(String[] args) {
+    ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+    WebServer webServer =
+        serverFactory.getWebServer(
+            servletContext ->
+                servletContext
+                    .addServlet(
+                        "hello",
+                        new HttpServlet() {
+                          @Override
+                          protected void service(HttpServletRequest req, HttpServletResponse resp)
+                              throws IOException {
+                            resp.setStatus(200);
+                            resp.setHeader("Content-Type", "text/plain");
+                            resp.getWriter().println("Hello Servlet");
+                          }
+                        })
+                    .addMapping("/hello"));
+    webServer.start();
+  }
 }
