b829fadefd8469696c545bc730eb5480782309f5
p829911 <p829911@gmail.com>
2024-05-06 00:20:19 +0900

    introduce GenericApplicationContext and register bean

--- src/main/java/me/p829911/helloboot/HellobootApplication.java
+++ src/main/java/me/p829911/helloboot/HellobootApplication.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.http.HttpHeaders;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -15,12 +15,14 @@ import org.springframework.http.MediaType;
 public class HellobootApplication {
 
   public static void main(String[] args) {
+    GenericApplicationContext applicationContext = new GenericApplicationContext();
+    applicationContext.registerBean(HelloController.class);
+    applicationContext.refresh();
+
     ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
     WebServer webServer =
         serverFactory.getWebServer(
-            servletContext ->
-            {
-              HelloController helloController = new HelloController();
+            servletContext -> {
               servletContext
                   .addServlet(
                       "frontcontroller",
@@ -32,10 +34,12 @@ public class HellobootApplication {
                           if (req.getRequestURI().equals("/hello")
                               && req.getMethod().equals(HttpMethod.GET.name())) {
                             String name = req.getParameter("name");
+
+                            HelloController helloController = applicationContext.getBean(
+                                HelloController.class);
                             String ret = helloController.hello(name);
 
-                            resp.setStatus(HttpStatus.OK.value());
-                            resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                            resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
                             resp.getWriter().println(ret);
                           } else {
                             resp.setStatus(HttpStatus.NOT_FOUND.value());
