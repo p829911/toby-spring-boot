b2d42918da6e951a413698d498fce126f37327a8
p829911 <p829911@gmail.com>
2024-05-15 21:17:58 +0900

    합성 애노테이션의 적용

---
+++ src/main/java/me/p829911/helloboot/MySpringBootAnnotation.java
@@ -0,0 +1,14 @@
+package me.p829911.helloboot;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Configuration
+@ComponentScan
+public @interface MySpringBootAnnotation {}
