0cd2b90b96b4862bf934c203cc9d14aaf7d33c17
p829911 <p829911@gmail.com>
2024-06-01 23:50:43 +0900

    자동 구성 정보 파일 분리

--- src/main/java/me/p829911/config/MyAutoConfigImportSelector.java
+++ src/main/java/me/p829911/config/MyAutoConfigImportSelector.java
@@ -1,15 +1,31 @@
 package me.p829911.config;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
 
-	@Override
-	public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-		return new String[] {
-				"me.p829911.config.autoconfig.DispatcherServletConfig",
-				"me.p829911.config.autoconfig.TomcatWebServerConfig"
-		};
-	}
+  private final ClassLoader classLoader;
+
+  public MyAutoConfigImportSelector(ClassLoader classLoader) {
+    this.classLoader = classLoader;
+  }
+
+  @Override
+  public String[] selectImports(AnnotationMetadata importingClassMetadata) {
+    List<String> autoConfigs = new ArrayList<>();
+    ImportCandidates.load(MyAutoConfiguration.class, classLoader).forEach(autoConfigs::add);
+    //    for (String candidate : ImportCandidates.load(me.p829911.config.MyAutoConfiguration.imports.class, classLoader)) {
+    //      autoConfigs.add(candidate);
+    //    }
+    return autoConfigs.toArray(String[]::new);
+    //    return Arrays.copyOf(autoConfigs.toArray(), autoConfigs.size(), String[].class);
+    //    return autoConfigs.toArray(new String[0]);
+    //    Iterable<String> candidates = ImportCandidates.load(me.p829911.config.MyAutoConfiguration.imports.class,
+    // classLoader);
+    //    return StreamSupport.stream(candidates.spliterator(), false).toArray(String[]::new);
+  }
 }
