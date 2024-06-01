cee7350f9f6b0b3f883bf1f5f8c60177989b3709
p829911 <p829911@gmail.com>
2024-06-01 23:56:54 +0900

    자동 구성 애노테이션 적용

--- src/main/java/me/p829911/config/MyAutoConfiguration.java
+++ src/main/java/me/p829911/config/MyAutoConfiguration.java
@@ -8,5 +8,5 @@ import org.springframework.context.annotation.Configuration;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public @interface MyAutoConfiguration {}
--- src/main/java/me/p829911/config/autoconfig/DispatcherServletConfig.java
+++ src/main/java/me/p829911/config/autoconfig/DispatcherServletConfig.java
@@ -1,10 +1,10 @@
 package me.p829911.config.autoconfig;
 
+import me.p829911.config.MyAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
+@MyAutoConfiguration
 public class DispatcherServletConfig {
 
 	@Bean
