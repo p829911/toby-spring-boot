97888d861431052c1639ed7736cebac2f57e0b98
p829911 <p829911@gmail.com>
2024-06-02 18:35:51 +0900

    스타터와 Jetty 서버 구성 추가

--- src/main/java/me/p829911/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/me/p829911/config/autoconfig/JettyWebServerConfig.java
@@ -1,15 +1,15 @@
 package me.p829911.config.autoconfig;
 
 import me.p829911.config.MyAutoConfiguration;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
 @MyAutoConfiguration
-public class TomcatWebServerConfig {
+public class JettyWebServerConfig {
 
-	@Bean("tomcatWebServerFactory")
+	@Bean("jettyWebServerFactory")
 	public ServletWebServerFactory servletWebServerFactory() {
-		return new TomcatServletWebServerFactory();
+    return new JettyServletWebServerFactory();
 	}
 }
