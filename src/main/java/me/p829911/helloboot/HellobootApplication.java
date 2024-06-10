8cc09d7cdb98c1a20916ccc495cecef99148be55
p829911 <p829911@gmail.com>
2024-06-10 22:02:39 +0900

    ApplicationRunner로 실행 시 Environment 확인 Environment 우선 순위 1. System Properties: vm Option 2. Environment Variables 3. application.properties

--- src/main/java/me/p829911/helloboot/HellobootApplication.java
+++ src/main/java/me/p829911/helloboot/HellobootApplication.java
@@ -1,10 +1,21 @@
 package me.p829911.helloboot;
 
 import me.p829911.config.MySpringBootApplication;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @MySpringBootApplication
 public class HellobootApplication {
+
+  @Bean
+  ApplicationRunner applicationRunner(Environment env) {
+    return args -> {
+      String name = env.getProperty("my.name");
+      System.out.println("my.name: " + name);
+    };
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(HellobootApplication.class, args);
